2d63bd789f7c4416697b6074640d7a90e8a0a3ea
hmnarciso77 <hmnarciso77@gmail.com>
2025-10-02 15:48:09 +0100

    Create exclusiveOptionsMatrix.js

---
+++ matrix/exclusiveOptionsMatrix.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('input:checkbox').click(function () {
+    const qid = $(this).attr("id").split('_')[0];
+    const i = $(this).attr("id").split('_')[3];
+    const exclusiveOption = '8'; // Corrigido: nome de variável válido e valor como string
+
+    if ($(this).attr('id') === `${qid}_A_${exclusiveOption}_${i}`) {
+      // Se a opção exclusiva foi clicada, desmarca todas as outras
+      $(`input[id$="_${i}"]:checkbox`).not(this).prop('checked', false);
+    } else {
+      // Se outra opção foi clicada, desmarca a exclusiva
+      $(`#${qid}_A_${exclusiveOption}_${i}`).prop('checked', false);
+    }
+  });
+});
